9ec30ab5a78498a93346b32dbb9015cce0b81602
TAksenov <taksenov@gmail.com>
2017-12-03 19:29:07 +0500

    add base fuctional of AuthPage

--- src/components/AppRouter/AppRouter.js
+++ src/components/AppRouter/AppRouter.js
@@ -2,18 +2,16 @@ import React, { Component } from 'react';
 import { Route, Switch, Redirect } from 'react-router-dom';
 
 import PrivateRoute from '../PrivateRoute';
-
-// TODO: Убрать заглушку!
-let PrivatePage = '';
-let LoginPage = '';
+import AuthPage from '../AuthPage';
+import UserPage from '../UserPage';
 
 class AppRouter extends Component {
     render() {
         return (
             <div className="App">
                 <Switch>
-                    <Route path="/login" exact component={LoginPage} />
-                    <PrivateRoute path="/user/:name" component={PrivatePage} />
+                    <Route path="/login" exact component={AuthPage} />
+                    <PrivateRoute path="/user/:name" component={UserPage} />
                     <Redirect to="/user/dex157" />
                 </Switch>
             </div>
--- src/components/AuthPage/AuthPage.js
+++ src/components/AuthPage/AuthPage.js
@@ -1,8 +1,57 @@
 import React, { Component } from 'react';
 
+import { authorize, logout } from '../../actions/auth';
+
+import './AuthPage.css';
+
 class AuthPage extends Component {
+    constructor(props) {
+        super(props);
+
+        this.state = {
+            inputToken: ''
+        };
+
+        this.handleChangeInput = this.handleChangeInput.bind(this);
+        this.handleKeyDown = this.handleKeyDown.bind(this);
+    }
+
+    handleChangeInput(e) {
+        const { value } = e.target;
+        this.setState({ inputToken: value });
+    } //handleChangeInput
+
+    handleKeyDown(e) {
+        const { inputToken } = this.state;
+        if (e.keyCode === 13 && inputToken) {
+            console.log(inputToken);
+            authorize(inputToken);
+        }
+    } //handleKeyDown
+    //fb630bc15d7520e0a5a9354dec3d20d9b347911c
+
     render() {
-        return null;
+        const { inputToken } = this.state;
+        return (
+            <div className="AuthPage">
+                <p>
+                    Получить токен нужно на своей странице github, перейдите по{' '}
+                    <a href="https://github.com/settings/tokens">адресу</a> и
+                    создать себе токен. Запишите куда нибудь токен, так как
+                    после создания доступ к нему будет только один раз.
+                </p>
+                <br />
+                <input
+                    placeholder="Введите токен"
+                    type="text"
+                    onChange={this.handleChangeInput}
+                    value={inputToken}
+                    onKeyDown={this.handleKeyDown}
+                />
+                <br />
+                <p>После ввода нажать Enter</p>
+            </div>
+        );
     }
 }
 
