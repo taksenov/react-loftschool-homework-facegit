19509e4028a38fc3a283f5e4553b3602260691f7
TAksenov <taksenov@gmail.com>
2017-12-05 17:38:17 +0500

    some ref in AppRouter.test v2

--- src/components/AppRouter/__tests__/AppRouter.test.js
+++ src/components/AppRouter/__tests__/AppRouter.test.js
@@ -8,24 +8,24 @@ describe('AppRouter >', () => {
     const renderedComponent = shallow(<AppRouter />);
     // // Выведем отрендеренный компонент
     // console.log(renderedComponent.debug());
-    it('should render AppRouter > Switch', () => {
+    it('should render Switch', () => {
         expect(renderedComponent.find('Switch')).toBeTruthy();
     });
-    it('should render AppRouter > PrivateRoute with path="/user/:name"', () => {
+    it('should render PrivateRoute with path="/user/:name"', () => {
         expect(
             renderedComponent.find('Connect(PrivateRoute)').find({
                 path: '/user/:name'
             })
         ).toBeTruthy();
     });
-    it('should render AppRouter > Route with path="/login"', () => {
+    it('should render Route with path="/login"', () => {
         expect(
             renderedComponent.find('Route').find({
                 path: '/login'
             })
         ).toBeTruthy();
     });
-    it('should render AppRouter > Redirect to="/user/dex157"', () => {
+    it('should render Redirect to="/user/dex157"', () => {
         expect(
             renderedComponent.find('Redirect').find({
                 to: '/user/dex157'
