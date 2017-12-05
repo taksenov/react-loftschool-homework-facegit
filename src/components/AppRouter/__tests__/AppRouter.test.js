e92f017d417e24c3f7f00db192727c667d18aeff
TAksenov <taksenov@gmail.com>
2017-12-05 17:16:50 +0500

    some ref in AppRouter.test

--- src/components/AppRouter/__tests__/AppRouter.test.js
+++ src/components/AppRouter/__tests__/AppRouter.test.js
@@ -5,17 +5,13 @@ import { Switch, Route, Redirect } from 'react-router-dom';
 import AppRouter from '../AppRouter';
 
 describe('AppRouter >', () => {
+    const renderedComponent = shallow(<AppRouter />);
+    // // Выведем отрендеренный компонент
+    // console.log(renderedComponent.debug());
     it('should render AppRouter > Switch', () => {
-        const renderedComponent = shallow(<AppRouter />);
-
-        // // Выведем отрендеренный компонент
-        // console.log(renderedComponent.debug());
-
         expect(renderedComponent.find('Switch')).toBeTruthy();
     });
     it('should render AppRouter > PrivateRoute with path="/user/:name"', () => {
-        const renderedComponent = shallow(<AppRouter />);
-
         expect(
             renderedComponent.find('Connect(PrivateRoute)').find({
                 path: '/user/:name'
@@ -23,8 +19,6 @@ describe('AppRouter >', () => {
         ).toBeTruthy();
     });
     it('should render AppRouter > Route with path="/login"', () => {
-        const renderedComponent = shallow(<AppRouter />);
-
         expect(
             renderedComponent.find('Route').find({
                 path: '/login'
@@ -32,8 +26,6 @@ describe('AppRouter >', () => {
         ).toBeTruthy();
     });
     it('should render AppRouter > Redirect to="/user/dex157"', () => {
-        const renderedComponent = shallow(<AppRouter />);
-
         expect(
             renderedComponent.find('Redirect').find({
                 to: '/user/dex157'
